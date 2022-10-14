53e8ab3c8bb137f68d8f8e2c73eff84782e3528a
plettboy <ryan.plett@yahoo.com>
2022-10-14 20:01:58 +0000

    websocket iplemented, routing finished

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,47 +1,83 @@
+
+
 import React, { Component } from "react";
 import Loading from "./Loading";
+import Panel from "./Panel";
 import classnames from "classnames";
-import Panel from "./Panel"
 
-/////////// FAKE DATA //////////
+import axios from "axios";
+
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+} from "helpers/selectors";
+
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
 class Dashboard extends Component {
   state = {
-    loading: false,
-    focused: null
-  };
-
-  selectPanel(id) {
-    this.state.focused ? this.setState({ focused: null }) : this.setState({ focused: id });
+    loading: true,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {}
   }
 
-  focusState(id) {
+  focusPanel(id) {
     this.setState(previousState => ({
       focused: previousState.focused !== null ? null : id
     }));
   }
+
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
+  };
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  };
 
 
   render() {
@@ -52,14 +88,15 @@ class Dashboard extends Component {
     if (this.state.loading) {
       return <Loading />;
     }
+
     const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
       .map(panel => (
         <Panel
           key={panel.id}
           id={panel.id}
           label={panel.label}
-          value={panel.value}
-          onSelect={event => this.focusState(panel.id)}
+          value={panel.getValue(this.state)}
+          onSelect={event => this.focusPanel(panel.id)}
         />
       ));
 
