2d57f4582708c95d5dfc66c107894071d2512d4f
plettboy <ryan.plett@yahoo.com>
2022-10-14 19:33:49 +0000

    handled the state

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,26 +29,43 @@ const data = [
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
 
+  selectPanel(id) {
+    this.state.focused ? this.setState({ focused: null }) : this.setState({ focused: id });
+  }
+
+  focusState(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
+
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={event => this.focusState(panel.id)}
+        />
+      ));
 
-    const panels = data.map(panel => (
-      <Panel
-        key={panel.id}
-        id={panel.id}
-        label={panel.label}
-        value={panel.value}
-      />
-    ));
-
-    return <main className={dashboardClasses}>{panels}</main>;
+    return <main className={dashboardClasses}>
+      {panels}
+    </main>
   }
 }
 
